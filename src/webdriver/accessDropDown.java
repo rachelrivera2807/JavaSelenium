f9644ad5de1b96de555bd2d00e930fe6a0bf8cf9
ThuyHien <thuyhiendoremi@gmail.com>
2023-04-03 11:40:23 +0700

    Add accessDropDown

---
+++ src/webdriver/accessDropDown.java
@@ -0,0 +1,23 @@
+package webdriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class accessDropDown {
+    public static void main(String[] args) {
+        System.setProperty("web-driver.chrome.driver", "/browserDrivers/IEDriverServer.exe");
+        String baseURL = "https://demo.guru99.com/test/newtours/register.php";
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseURL);
+
+        Select drpCountry = new Select(driver.findElement(By.name("country")));
+        drpCountry.selectByVisibleText("ANTARCTICA");
+
+        //Selecting Items in a Multiple SELECT elements
+        driver.get("http://jsbin.com/osebed/2");
+        Select fruits = new Select(driver.findElement(By.id("fruits")));
+        fruits.selectByVisibleText("Banana");
+        fruits.selectByIndex(1);
+    }
+}
