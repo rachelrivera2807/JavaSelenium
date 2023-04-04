d3e911977a5ea1933339b5c7436cfdcf0f23526b
ThuyHien <thuyhiendoremi@gmail.com>
2023-04-04 14:05:25 +0700

    Add UpAndDownLoad

---
+++ src/webdriver/UpAndDownLoad.java
@@ -0,0 +1,46 @@
+package webdriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+import java.io.IOException;
+public class UpAndDownLoad {
+    public static void main(String[] args) {
+        System.setProperty("web- driver.chrome.driver","/browserDrivers/IEDriverServer.exe");
+        /*String baseUrl = "https://demo.guru99.com/test/upload/";
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+        WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
+
+        // enter the file path onto the file-selection input field
+        uploadElement.sendKeys("C:\\Users\\nk\\IdeaProjects\\JavaSelenium");
+
+        // check the "I accept the terms of service" check box
+        driver.findElement(By.id("terms")).click();
+
+        // click the "UploadFile" button
+        driver.findElement(By.name("send")).click();*/
+
+        String baseUrl = "https://demo.guru99.com/test/yahoo.html";
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+        WebElement downloadButton = driver.findElement(By
+                .id("messenger-download"));
+        String sourceLocation = downloadButton.getAttribute("href");
+        String wget_command = "cmd /c C:\\Wget\\wget.exe -P D: --no-check-certificate " + sourceLocation;
+
+        try {
+            Process exec = Runtime.getRuntime().exec(wget_command);
+            int exitVal = exec.waitFor();
+            System.out.println("Exit value: " + exitVal);
+        } catch (InterruptedException | IOException ex) {
+            System.out.println(ex.toString());
+        }
+        driver.close();
+    }
+
+}
+
