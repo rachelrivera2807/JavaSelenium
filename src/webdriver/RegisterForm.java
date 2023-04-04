15b8c7d8f2ff65d707df3fb94d9162bd7f4028f2
ThuyHien <thuyhiendoremi@gmail.com>
2023-04-04 09:06:03 +0700

    Edit accessDropDown

---
+++ src/webdriver/RegisterForm.java
@@ -0,0 +1,36 @@
+package webdriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegisterForm {
+    public static void main(String[] args) {
+        System.setProperty("web-driver.chrome.driver", "/browserDrivers/IEDriverServer.exe");
+        String baseURL = "https://demo.automationtesting.in/Register.html";
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseURL);
+        WebElement first_name = driver.findElement(By.cssSelector("[type='text'][placeholder='First Name']"));
+        first_name.sendKeys("Thuy");
+        WebElement last_name = driver.findElement(By.cssSelector("[type='text'][placeholder='Last Name']"));
+        last_name.sendKeys("Hien");
+        WebElement address = driver.findElement(By.cssSelector("#basicBootstrapForm > div:nth-child(2) > div > textarea"));
+        address.sendKeys("149 Hà Huy Tập");
+        WebElement email = driver.findElement(By.cssSelector("#eid > input"));
+        email.sendKeys("thuyhiendoremi@gmail.com");
+        WebElement phone = driver.findElement(By.cssSelector("#basicBootstrapForm > div:nth-child(4) > div > input"));
+        phone.sendKeys("0376763365");
+        WebElement gender = driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[5]/div/label[2]/input"));
+        gender.click();
+        WebElement hobbies = driver.findElement((By.xpath("//*[@id=\"checkbox2\"]")));
+        hobbies.click();
+        WebElement languages = driver.findElement(By.xpath("//*[@id=\"msdd\"]"));
+        Actions action = new Actions(driver);
+        action.click(languages).build().perform();
+        Select dropdown = new Select(languages);
+        dropdown.selectByVisibleText("English");
+    }
+}
--- src/webdriver/accessDropDown.java
+++ src/webdriver/accessDropDown.java
@@ -6,18 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 public class accessDropDown {
     public static void main(String[] args) {
-        System.setProperty("web-driver.chrome.driver", "/browserDrivers/IEDriverServer.exe");
+        /*System.setProperty("web-driver.chrome.driver", "/browserDrivers/IEDriverServer.exe");
         String baseURL = "https://demo.guru99.com/test/newtours/register.php";
         WebDriver driver = new ChromeDriver();
         driver.get(baseURL);
 
         Select drpCountry = new Select(driver.findElement(By.name("country")));
-        drpCountry.selectByVisibleText("ANTARCTICA");
-
+        drpCountry.selectByVisibleText("ANTARCTICA");*/
         //Selecting Items in a Multiple SELECT elements
-        driver.get("http://jsbin.com/osebed/2");
-        Select fruits = new Select(driver.findElement(By.id("fruits")));
-        fruits.selectByVisibleText("Banana");
-        fruits.selectByIndex(1);
+        //driver.get("http://jsbin.com/osebed/2");
+        //Select fruits = new Select(driver.findElement(By.id("fruits")));
+        //fruits.selectByVisibleText("Banana");
+        //fruits.selectByIndex(1);
+            System.setProperty("web- driver.chrome.driver", "/browserDrivers/IEDriverServer.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.traveloka.com/vi-vn/");
+
+            driver.findElement(By.xpath(".//*[@id='multiSearchContainerTabs']/li[1]/a")).click();
+            driver.findElement(By.xpath( ".//*[@id='tvDesktop-flightSearchForm']/div/div/div/div/div/div[1]/div/div/div/ul/li[1]/div/div[1]/div/div[1]/label/div/div/div[2]/div[2]/input")).clear();
+            driver.findElement(By.xpath(".//*[@id='tvDesktop-flightSearchForm']/div/div/div/div/div/div[1]/div/div/div/ul/li[1]/div/div[1]/div/div[2]/div/div/div[2]/div[2]/table/tbody/tr[4]/td[1]/a")).click();
     }
 }
