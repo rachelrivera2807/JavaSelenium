17e72de644f76a043a7d395263aa6cbfb3ad163a
ThuyHien <thuyhiendoremi@gmail.com>
2023-04-06 09:33:35 +0700

    Edit firsttestngpackage

--- src/firsttestngpackage/firsttestngfile.java
+++ src/firsttestngpackage/firsttestngfile.java
@@ -4,22 +4,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
+@Test
 public class firsttestngfile {
     public String baseUrl = "http://demo.guru99.com/test/newtours/";
     String driverPath = "C:\\browserDrivers\\IEDriverServer.exe";
-    public WebDriver driver ;
-
-    @Test
-    public void verifyHomepageTitle() {
-
+    public WebDriver driver;
+    public String expected = null;
+    public String actual = null;
+    @BeforeTest
+    public void launchBrowser() {
         System.out.println("launching chrome browser");
         System.setProperty("web-driver.chrome.driver", driverPath);
-        driver = new ChromeDriver();
+        driver= new ChromeDriver();
         driver.get(baseUrl);
+    }
+
+    @BeforeMethod
+    public void verifyHomepageTitle() {
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test(priority = 0)
+    public void register(){
+        driver.findElement(By.linkText("REGISTER")).click() ;
+        expected = "Register: Mercury Tours";
+        actual = driver.getTitle();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @AfterMethod
+    public void goBackToHomepage ( ) {
+        driver.findElement(By.linkText("Home")).click() ;
+    }
+
+    @AfterTest
+    public void terminateBrowser(){
         driver.close();
     }
 }
