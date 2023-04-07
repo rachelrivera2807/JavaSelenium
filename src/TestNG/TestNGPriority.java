aca76d352bf9b0dee7eabf45f3ae9a5ad78912fc
ThuyHien <thuyhiendoremi@gmail.com>
2023-04-07 14:35:50 +0700

    Add TestNGPriority

---
+++ src/TestNG/TestNGPriority.java
@@ -0,0 +1,35 @@
+package TestNG;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class TestNGPriority {
+    WebDriver driver;
+
+    // Method 1: Open Browser say Firefox
+    @Test (priority=1)
+    public void openBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    // Method 2: Launch Google.com
+    @Test (priority=2)
+    public void launchGoogle() {
+        driver.get("http://www.google.co.in");
+    }
+
+    // Method 3: Perform a search using "Facebook"
+    @Test (priority=3)
+    public void peformSeachAndClick1stLink() {
+        driver.findElement(By.xpath(".//*[@title='Search']")).sendKeys("Facebook");
+    }
+
+    // Method 4: Verify Google search page title.
+    @Test (priority=4)
+    public void FaceBookPageTitleVerification() throws Exception {
+        driver.findElement(By.xpath(".//*[@value='Search']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.getTitle().contains("Facebook - Google Search"), true);
+    }
+}
